0856910c2e5f034967448c915243be6645e3913a
yong <yong@yongdeMacBook-Air.local>
2021-09-25 13:07:42 +0800

    空接口

--- e_interface/main.go
+++ e_interface/main.go
@@ -68,4 +68,20 @@ func main() {
 	//fmt.Printf("%T", goods)
 	totalPrice := calculateAllPrice(goods)
 	fmt.Println("total price: ", totalPrice)
+
+	// 空接口
+	// 空接口是指没有定义任何方法的接口。因此任何类型都实现了空接口。
+	// 空接口类型的变量可以存储任意类型的变量。
+
+	// 定义一个空接口x
+	var x interface{}
+	s := "Hello 沙河"
+	x = s
+	fmt.Printf("type:%T value:%v\n", x, x)
+	i := 100
+	x = i
+	fmt.Printf("type:%T value:%v\n", x, x)
+	b := true
+	x = b
+	fmt.Printf("type:%T value:%v\n", x, x)
 }
