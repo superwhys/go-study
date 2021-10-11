b61a802583d1baf9ebca7af48e00806cbedddcd2
Superyong <269085434@qq.com>
2021-10-11 10:36:14 +0800

    'protobug'

--- v_protobuf/main.go
+++ v_protobuf/main.go
@@ -12,26 +12,17 @@ func main() {
 		Ids:                  123,
 		Name:                 "SuerYong",
 		Number:               "123456",
-		XXX_NoUnkeyedLiteral: struct{}{},
-		XXX_unrecognized:     nil,
-		XXX_sizecache:        0,
 	}
 
 	data, _ := proto.Marshal(&p1)
 
-	fmt.Printf("%s\n", data)
-	fmt.Printf("%T\n", data)
+	fmt.Printf("%+v\n", p1)
+	fmt.Printf("%v\n", data)
 
 	err := ioutil.WriteFile("./proto.dat", data, 0644)
 	if err != nil {
 		return
 	}
-
-	//data2, err := ioutil.ReadFile("./proto.dat")
-	//if err != nil {
-	//	fmt.Printf("read file failed, err:%v\n", err)
-	//	return
-	//}
 
 	var p2 person.Person
 
